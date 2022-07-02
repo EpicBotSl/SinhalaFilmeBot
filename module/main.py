09f6d3a88990800d415959966ef167f64fac5daa
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-02 17:45:09 +0530

    Update main.py

--- module/main.py
+++ module/main.py
@@ -66,7 +66,7 @@ async def startprivates(client, message):
             )
         else:
             logging.info(f"#NewUser :- Name : {message.from_user.first_name} ID : {message.from_user.id}")
-    file_id = "BAADBQADjAUAAgJVAAFWhDusR8sFJ4EC"
+    file_id = "https://telegra.ph/file/64865d56582fa87eba003.mp4"
     await client.send_video(message.chat.id, file_id)
     text = f"Hi {message.from_user.mention}, Welcome To Epic Film Bot 📽️"
     reply_markup = ST_BTN
