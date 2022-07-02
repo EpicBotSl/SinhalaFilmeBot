2c2086c994184c3b087c1dd138ef5d1d1fff778f
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-02 20:37:29 +0530

    Update main.py

--- module/main.py
+++ module/main.py
@@ -17,6 +17,7 @@ import datetime
 import os
 import random
 import logging
+from script import *
 from pyrogram.errors.exceptions.bad_request_400 import *
 from pyrogram.errors import *
 from pyrogram.types import *
@@ -107,28 +108,6 @@ ST_BTN = InlineKeyboardMarkup([[
             ]
         ])
 
-STAT_STICKER = ["CAACAgQAAxkBAAEFHRditZFgRBAPm-9bkFJUQKOjSEgxoQACfwsAAmgpeVF2roP_0GLhzykE",
-                "CAACAgQAAxkBAAEFHRVitZFYQ_EPOF7Y1GenAAHZOfu6xNIAAj4MAAKd3llQRh5-qJlCwa0pBA",
-                "CAACAgQAAxkBAAEFHRNitZFVEBwdq0uFJDOvDRx2IzdoCwAC5wwAAubdSFEk6BkQ4EbQ1ikE",
-                "CAACAgQAAxkBAAEFHRFitZFRwzQPYrVUQkxVP4yxF2Uw3gAC4AkAAu9GYFGTgHavjO_HLikE",
-                "CAACAgQAAxkBAAEFHQ9itZFNixLf7fEZICaK8DF-Li967wACUAwAAmEq4VF8xFsUvkvQXSkE"              
-         ]  
-
-START = f"""
-𝒉𝒆𝒍𝒍𝒐 𝒑𝒆𝒐𝒑𝒍𝒆𝒔😌
-
-ᚖ𝑰 𝒂𝒎 𝒇𝒂𝒔𝒕 & 𝑺𝒊𝒎𝒑𝒍𝒆 𝑺𝒊𝒏𝒉𝒂𝒍𝒂 & 𝑬𝒏𝒈𝒍𝒊𝒔𝒉 𝑭𝒊𝒍𝒎𝒆 𝒅𝒐𝒘𝒏𝒍𝒐𝒂𝒅𝒆𝒓 𝒃𝒐𝒕ᚖ
-
-ᨖ𝑻𝒉𝒊𝒔 𝑩𝒐𝒕 𝒂𝒍𝒔𝒐 𝑰𝒏𝒍𝒊𝒏𝒆 & 𝒈𝒓𝒐𝒖𝒑 𝒔𝒖𝒑𝒑𝒐𝒓𝒕𝒆𝒅
-
-ᨖ𝒀𝒐𝒖 𝑪𝒂𝒏 𝒈𝒆𝒕 𝒂𝒏𝒚 𝑭𝒊𝒍𝒎𝒆 𝒇𝒓𝒐𝒎 𝒖𝒔𝒊𝒏𝒈 𝑻𝒉𝒊𝒔 𝒃𝒐𝒕
-
-ᚔᚔᚔᚔᚔᚔᚔᚔᚔᚔᚔᚔᚔᚔᚔᚔᚔᚔᚔ
-ᚗDeveloperᚋ [ɴᴀᴠᴀɴᴊᴀɴᴀ](https://t.me/NA_VA_N_J_NA1)
-ᚗPowerd byᚋ [Epic Developers Community🇱🇰](https://t.me/EpicBotsSl)
-ᚔᚔᚔᚔᚔᚔᚔᚔᚔᚔᚔᚔᚔᚔᚔᚔᚔᚔᚔ
-•`𝓌ℯ𝓁𝒸ℴ𝓂ℯ 𝓉ℴ 𝓉𝒽ℯ 𝓃ℯ𝓌 𝒶ℊℯ`
-"""
 
 #=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•Epic Bots 2022© All Rights Resived•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=#
 print("Commands.py Started🔥🌹")
