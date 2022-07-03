2cbdff8d45421b071b1c2bbf04ea3da95f8a799d
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-03 10:10:37 +0530

    Create send.py

---
+++ filmedb/send.py
@@ -0,0 +1,79 @@
+import re
+import uuid
+import socket
+import platform
+import time
+import math
+import json
+import string
+import traceback
+import psutil
+import asyncio
+import wget
+import motor.motor_asyncio
+import pymongo
+import aiofiles
+import datetime
+import os
+import random
+import logging
+from pyrogram.errors.exceptions.bad_request_400 import *
+from pyrogram.errors import *
+from pyrogram.types import *
+from helper.decorators import humanbytes
+from asyncio import *
+import requests
+from filmedb.database import Database
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import *
+from pyrogram.types import Message
+
+from info import START_MSG, CHANNELS, ADMINS, INVITE_MSG, DATABASE_URI, PRIVATE_LOG
+from utils import Media, unpack_new_file_id
+
+async def send_msg(user_id, message):
+    try:
+        await message.copy(chat_id=user_id)
+        return 200, None
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return send_msg(user_id, message)
+    except InputUserDeactivated:
+        return 400, f"{user_id} : deactivated\n"
+    except UserIsBlocked:
+        return 400, f"{user_id} : user is blocked\n"
+    except PeerIdInvalid:
+        return 400, f"{user_id} : user id invalid\n"
+    except Exception as e:
+        return 500, f"{user_id} : {traceback.format_exc()}\n"
+
+@Client.on_message(filters.command("send"))
+async def status(bot, message):
+    if message.from_user.id not in ADMINS:
+        await message.delete()
+        return
+    mesg=message.reply_to_message
+    f= message.text
+    s=f.replace('/send ' ,'')
+    fid=s.replace('%20', ' ')
+    await send_msg(user_id=fid, message=mesg)
+    await message.delete()
+    await bot.send_message(message.chat.id, text=f"Ur Msg Sent To [User](tg://user?id={fid})", reply_markup=CLOSE_BUTTON)
+
+@Client.on_message(filters.command("send"))
+async def statns(bot, message):
+    if message.from_user.id not in ADMINS:
+        await message.delete()
+        return
+    mesg=message.reply_to_message
+    f= message.text
+    s=f.replace('/send ' ,'')
+    fid=s.replace('%20', ' ')
+    await send_message(user_id=fid, message=message)
+    await message.delete()
+    await bot.send_message(message.chat.id, text=f"Ur Msg Sent To [User](tg://user?id={fid})", reply_markup=CLOSE_BUTTON)
+    await bot.send_message(PRIVATE_LOG, text="Massage Sended To User⚡")
+    await send_message(PRIVATE_LOG, message=message)
+
+print("Send Py Started Successfully 🔥")
