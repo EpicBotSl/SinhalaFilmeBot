061239ee7d09def6ca85f0d9772b4841af00216e
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-03 10:17:31 +0530

    Create callback.py

---
+++ filmedb/callback.py
@@ -0,0 +1,51 @@
+import re
+import uuid
+import socket
+import platform
+import time
+import math
+import json
+import string
+import traceback
+import psutil
+import asyncio
+import wget
+import motor.motor_asyncio
+import pymongo
+import aiofiles
+import datetime
+import os
+import random
+import logging
+from script import *
+from pyrogram.errors.exceptions.bad_request_400 import *
+from pyrogram.errors import *
+from pyrogram.types import *
+from helper.decorators import humanbytes
+from asyncio import *
+import requests
+from filmedb.database import Database
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import *
+from pyrogram.types import Message
+
+from info import START_MSG, CHANNELS, ADMINS, INVITE_MSG, DATABASE_URI, PRIVATE_LOG
+from utils import Media, unpack_new_file_id
+
+
+#=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•Epic Bots 2022© All Rights Resived•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=#
+#Callbacks
+
+@Client.on_callback_query()  
+async def tgm(bot, update):  
+    if update.data == "add": 
+        await update.answer(
+             text="Epic Devs",
+        )  
+     elif update.data == "cloce":
+        await update.message.delete()
+
+#=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=
+
+print("Callback.py Started Successfully 🌱🔥")
